b51f15919282710b55bb214faf5300a58cf4bf8c
yihoney <109622@naver.com>
2023-01-02 21:22:50 +0900

    Initial commit

---
+++ src/main/java/com/example/yihoneyjenkinstest01/YihoneyJenkinsTest01Application.java
@@ -0,0 +1,13 @@
+package com.example.yihoneyjenkinstest01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YihoneyJenkinsTest01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YihoneyJenkinsTest01Application.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/yihoneyjenkinstest01/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.example.yihoneyjenkinstest01.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HomeController {
+    @GetMapping
+    public String doSomething() {
+        return "안녕하세요";
+    }
+}
