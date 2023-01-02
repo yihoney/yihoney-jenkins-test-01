f5d7e743d0f146d936be656f8a8fcbe164319e3c
yihoney <109622@naver.com>
2023-01-02 22:33:51 +0900

    jenkins 실행 테스트

--- src/main/java/com/example/yihoneyjenkinstest01/controller/HomeController.java
+++ src/main/java/com/example/yihoneyjenkinstest01/controller/HomeController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     @GetMapping
     public String doSomething() {
-        return "안녕하세요";
+        return "hihi";
     }
 }
